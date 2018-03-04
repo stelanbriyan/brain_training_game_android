fdb183abba3b576e2df608de0ca904f29af42061
stelan <stelan@sp-solutions.biz>
2018-03-04 09:03:08 +0530

    exit button added

--- app/src/main/java/org/incosyz/stelan/braintraininggame/HomeFragment.java
+++ app/src/main/java/org/incosyz/stelan/braintraininggame/HomeFragment.java
@@ -1,5 +1,7 @@
 package org.incosyz.stelan.braintraininggame;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,11 +10,57 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class HomeFragment extends Fragment {
+    private AlertDialog mDialog;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_home,
                 container, false);
+        View aboutButton = rootView.findViewById(R.id.about_button);
+
+        aboutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder =
+                        new AlertDialog.Builder(getActivity());
+                builder.setTitle(R.string.about_title);
+                builder.setMessage(R.string.about_text);
+                builder.setCancelable(false);
+                builder.setPositiveButton(R.string.ok_label,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                            }
+                        });
+                mDialog = builder.show();
+            }
+        });
+
+        View exitButton = rootView.findViewById(R.id.exit_button);
+        exitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setMessage("Do you want to exit?")
+                        .setNegativeButton(R.string.no_label,
+                                new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                                    }
+                                })
+                        .setPositiveButton(R.string.yes_label,
+                                new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        System.exit(0);
+                                    }
+                                })
+                        .show();
+            }
+        });
 
         return rootView;
     }
