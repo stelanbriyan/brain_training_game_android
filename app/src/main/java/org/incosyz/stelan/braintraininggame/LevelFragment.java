bc2ff96c016b7472f3c03489244b6e04ae5b5ae3
stelan <stelan@sp-solutions.biz>
2018-03-11 07:23:12 +0530

    play fragment and activity added

--- app/src/main/java/org/incosyz/stelan/braintraininggame/LevelFragment.java
+++ app/src/main/java/org/incosyz/stelan/braintraininggame/LevelFragment.java
@@ -1,5 +1,6 @@
 package org.incosyz.stelan.braintraininggame;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -7,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 public class LevelFragment extends Fragment {
@@ -16,12 +18,25 @@ public class LevelFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_level,
                 container, false);
 
-        Spinner spinner = (Spinner) rootView.findViewById(R.id.game_level_spinner);
+        Spinner spinner = rootView.findViewById(R.id.game_level_spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
                 R.array.game_levels, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
 
+        Button startButton = rootView.findViewById(R.id.start_button);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startGame();
+            }
+        });
+
         return rootView;
+    }
+
+    public void startGame() {
+        Intent intent = new Intent(getActivity(), PlayActivity.class);
+        getActivity().startActivity(intent);
     }
 }
---
+++ app/src/main/java/org/incosyz/stelan/braintraininggame/PlayFragment.java
@@ -0,0 +1,21 @@
+package org.incosyz.stelan.braintraininggame;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * @author Stelan Briyan
+ */
+public class PlayFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_play,
+                container, false);
+        return rootView;
+    }
+}
